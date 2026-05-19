be901e44433116243039aaa122e802eaa153fbec
Katherine Rush <108463876+krushy8@users.noreply.github.com>
2026-05-19 22:22:44 +0900

    added designs page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,6 +8,7 @@ const links = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About" },
   { href: "/projects", label: "Projects" },
+  { href: "/design", label: "Design" },
 ];
 
 export default function Nav() {
